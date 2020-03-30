f479fa081f76bc46f88f4c3cfde2e472139f0b62
Eva Ratna P <evaratnapalupi@outlook.com>
2020-03-30 15:08:55 +0700

    Seting routes di server js

--- koneksi.js
+++ koneksi.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 //buat koneksi database
 const conn = mysql.createConnection{
-    host:'localhost'
-    user:'root'
-    password:''
-    database:'dbrestapi'
+    host:'localhost';
+    user:'root';
+    password:'';
+    database:'dbrestapi';
 }
 
 conn.connect((err))=>{
