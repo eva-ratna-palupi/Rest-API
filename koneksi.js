1b0101c2d5935226b1d2b46e4a8e64238ac9a5f9
Eva Ratna P <evaratnapalupi@outlook.com>
2020-03-30 15:22:27 +0700

    Revisi kode koneksi.js

--- koneksi.js
+++ koneksi.js
@@ -1,16 +1,17 @@
 var mysql = require('mysql');
 
 //buat koneksi database
-const conn = mysql.createConnection{
-    host:'localhost';
-    user:'root';
-    password:'';
-    database:'dbrestapi';
-}
+const conn = mysql.createConnection(
+    {
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbrestapi'
+});
 
 conn.connect((err))=>{
     if(err) throw err;
-    console.log("Mysql terkoneksi");
-});;
+    console.log('Mysql terkoneksi');
+});
 
-module.exports = conn
+module.exports = conn;
